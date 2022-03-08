d0e938186b9ef33fd19d65832e1be969268b47ef
Bhargav <bhargav999reddy@gmail.com>
2022-03-09 00:19:20 +0530

    Button

--- src/atoms/Button/index.js
+++ src/atoms/Button/index.js
@@ -2,3 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Spacer } from "../Spacer";
+
+const StyledButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    cursor: pointer;
+
+    height: 
+`;
---
+++ src/tokens/buttons.js
@@ -0,0 +1,7 @@
+export const buttons = {
+  h: {
+    sm: "3",
+    rg: "4",
+    lg: "5",
+  },
+};
